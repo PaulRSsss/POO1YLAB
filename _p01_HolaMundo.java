328862598efc7d721c7c9303cb054c34b64c323f
PaulRSsss <pol97303@gmail.com>
2023-08-24 10:09:20 -0600

    Actividadad 1

--- _p01_HolaMundo.java
+++ _p01_HolaMundo.java
@@ -2,6 +2,13 @@
 
 public class _p01_HolaMundo {
     public static void main(String[] args) {
+        String amigo = "Juan";
+        String hermano = "Pablo";
+        String mensaje = String.format("Tanto %s como %S desean aprender a programar en Java", amigo, hermano);
+
         System.out.println("Hola Mundo en el Lenguaje Java");
+        System.out.println("\nHola amigo ");
+        System.out.println("\nMi amigo es " + amigo + " y mi hermaon es " + hermano + "\n");
+        System.out.println(mensaje);
     }   
 }
