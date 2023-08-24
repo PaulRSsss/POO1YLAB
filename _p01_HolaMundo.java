3b23bd6a92f0673ad0cd809a19ec483deda9b90b
PaulRSsss <pol97303@gmail.com>
2023-08-24 08:58:10 -0600

    va

---
+++ _p01_HolaMundo.java
@@ -0,0 +1,7 @@
+//Envía un saludo a la pantalla
+
+public class _p01_HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo en el Lenguaje Java");
+    }   
+}
