4294b80d738db263807ef0875edd71a42b007825
PaulRSsss <pol97303@gmail.com>
2023-09-27 23:46:30 -0600

    Actividad 14

--- _p06_PromedioCalificaciones.java
+++ _p06_PromedioCalificaciones.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class _p06_PromeidioCalificaciones {
+public class _p06_PromedioCalificaciones {
     public static void main(String[] args) {
         System.out.print("\033[H\033[2J"); System.out.flush();  // Borrar pantalla de la consola
 
