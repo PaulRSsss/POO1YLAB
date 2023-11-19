019ccc644a2a96c302f739c14c20bea1906860b2
PaulRSsss <pol97303@gmail.com>
2023-11-19 16:39:55 -0600

    Actividad 26

--- _p05_OperacionesMatematicas.java
+++ _p05_OperacionesMatematicas.java
@@ -8,7 +8,8 @@ public class _p05_OperacionesMatematicas {
         double x,y,suma,resta,mult,div,res,pot;
         x = 10.5;
         y = 2.5;
-        suma = x + y;
+        suma = x + y; 
+        
         resta = x - y;
         mult = x * y;
         div = x / y;
